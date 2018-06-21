e2fe092900f9dde9672c8615ef50a9ec93259401
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-06-20 20:48:51 -0400

    1D wave equation CPU solver generates and solves M problems.

--- basic-tests/simple-wave/cpu_wave_equation_1D.cpp
+++ basic-tests/simple-wave/cpu_wave_equation_1D.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <fstream>
+#include <random>
+
 #include <Eigen/Dense>
 
 using namespace std;
 using namespace Eigen;
 
-double normal_pdf(double x, double m, double s) {
-    static const double inv_sqrt_2pi = 0.3989422804014327;
-    double a = (x - m) / s;
+float normal_pdf(float x, float m, float s) {
+    static const float inv_sqrt_2pi = 0.3989422804014327;
+    float a = (x - m) / s;
 
-    return inv_sqrt_2pi / s * std::exp(-0.5f * a * a);
+    return (inv_sqrt_2pi / s) * std::exp(-0.5f * a * a);
 }
 
-int main() {
-    IOFormat CommaInitFmt(StreamPrecision, DontAlignCols, ", ", ", ", "", "", "", "");
-
-    ofstream outfile("wave_evolution.dat");  // Storing time-dependent solution.
+void solve_1D_wave_equation(double mu, double sigma) {
+    // IOFormat CommaInitFmt(StreamPrecision, DontAlignCols, ", ", ", ", "", "", "", "");
+    // ofstream outfile("wave_evolution.dat");  // Storing time-dependent solution.
 
     /* Problem parameters */
     double c = 1.0;  // Propagation speed of the wave.
@@ -28,7 +29,7 @@ int main() {
 
     double alpha = c*c * dt*dt / (2*dx*dx);
 
-    /* Discretize the wave equation using the Crank-Nicolson method and
+    /* We'll discretize the wave equation using the Crank-Nicolson method and
      * solve the resulting linear system A*u = b at every time step.
      */
     MatrixXf A(N,N);
@@ -48,15 +49,13 @@ int main() {
         A(i,i-1) = -alpha;
     }
 
-    cout << A << endl;
-
     // Set initial conditions.
     u_nm1(0)   = 0;
     u_nm1(N-1) = 0;
     for (int i = 1; i < N-1; i++)
-        u_nm1(i) = normal_pdf(i*dx, 0.5, 0.1);
+        u_nm1(i) = normal_pdf(i*dx, mu, sigma);
 
-    outfile << u_nm1.format(CommaInitFmt) << '\n';
+    // outfile << u_nm1.format(CommaInitFmt) << '\n';
 
     // Take the first time step.
     u_n(0)   = 0;
@@ -65,10 +64,13 @@ int main() {
     for (int i = 1; i < N-1; i++)
         u_n(i) = u_nm1(i) + (c*c/2) * (u_nm1(i+1) - 2*u_nm1(i) + u_nm1(i-1));
 
-    outfile << u_n.format(CommaInitFmt) << '\n';
+    // outfile << u_n.format(CommaInitFmt) << '\n';
 
-    double t = 0.0;
+    double t = dt; // We already took one step so t = dt now.
+
     while (t < t_end) {
+        t += dt;
+
         // Set up right-hand side vector b.
         b(0)   = 2*(1-alpha)*u_n(0) - u_nm1(0) + alpha*u_n(1);
         b(N-1) = 2*(1-alpha)*u_n(N-1) - u_nm1(N-1) + alpha*u_n(N-1);
@@ -82,11 +84,43 @@ int main() {
         u_np1(0)   = 0;
         u_np1(N-1) = 0;
 
-        outfile << u_np1.format(CommaInitFmt) << '\n';
+        // outfile << u_np1.format(CommaInitFmt) << '\n';
         
-        t += dt;
         u_n = u_np1;
     }
     
-    outfile.close();
+    // outfile.close();
 }
+
+void solve_1D_wave_equations(int M, float *mu, float *sigma) {
+    for (int i = 0; i < M; i++)
+        solve_1D_wave_equation(mu[i], sigma[i]);
+}
+
+int main() {
+    int M = 25;  // Number of problems to solve.
+
+    std::random_device rd;  // Obtain a random number generator from hardware.
+    std::mt19937 eng(rd()); // Seed the generator.
+
+    /* We will impose a Gaussian initial condition for the 1D wave equation, with randomly
+     * generated means and standard deviations.
+     */
+    std::uniform_int_distribution<> uniform_mu(0.2, 0.8);
+    std::uniform_int_distribution<> uniform_sigma(0.01, 0.5);
+
+    float *mu = new float[M];
+    float *sigma = new float[M];
+
+    for(int i = 0; i < M; i++) {
+        mu[i] = uniform_mu(eng);
+        sigma[i] = uniform_sigma(eng);
+    }
+
+    // Solve the M problems on the CPU.
+    solve_1D_wave_equations(M, mu, sigma);
+
+    delete [] mu;
+    delete [] sigma;
+}
+
